6e2a03f1b2bbaca548b5811c416dfbd60a357e89
pjonnathan <jonnathanmatos856@gmail.com>
2025-01-21 20:09:15 -0300

    feat: create repository Transaction

---
+++ src/main/java/com/example/picpaysimplificado/repository/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.example.picpaysimplificado.repository;
+
+import com.example.picpaysimplificado.domain.transaction.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
