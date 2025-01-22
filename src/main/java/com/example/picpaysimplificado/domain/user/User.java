4f4482817d9b9c062c6a119a69f7a00e92e8d6a3
pjonnathan <jonnathanmatos856@gmail.com>
2025-01-21 22:59:19 -0300

    modfield: Add new contruct

---
+++ src/main/java/com/example/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,112 @@
+package com.example.picpaysimplificado.domain.user;
+
+import com.example.picpaysimplificado.DTO.UserDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balace;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    public User() {
+    }
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.document = data.document();
+        this.email = data.email();
+        this.balace = data.balance();
+        this.password = data.password();
+        this.userType = data.userType();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalace() {
+        return balace;
+    }
+
+    public void setBalace(BigDecimal balace) {
+        this.balace = balace;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+}
