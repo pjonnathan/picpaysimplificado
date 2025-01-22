0ad1021a8ee640ed2609e3b678ab4d0c0ecd72da
pjonnathan <jonnathanmatos856@gmail.com>
2025-01-21 23:01:25 -0300

    fied: create Repository Transaction

--- src/main/java/com/example/picpaysimplificado/repository/TransactionRepository.java
+++ src/main/java/com/example/picpaysimplificado/repository/TransactionRepository.java
@@ -1,6 +1,6 @@
 package com.example.picpaysimplificado.repository;
 
-import com.example.picpaysimplificado.domain.transaction.transaction.Transaction;
+import com.example.picpaysimplificado.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
