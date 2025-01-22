2fa7fe7d8408d938e18a742bf248260f3d4d9696
pjonnathan <jonnathanmatos856@gmail.com>
2025-01-21 23:00:06 -0300

    fied: create User DTO

---
+++ src/main/java/com/example/picpaysimplificado/DTO/UserDTO.java
@@ -0,0 +1,8 @@
+package com.example.picpaysimplificado.DTO;
+
+import com.example.picpaysimplificado.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password, UserType userType) {
+}
